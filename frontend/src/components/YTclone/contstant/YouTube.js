64a2a89f1cc343313bae4cc4e4253ffaea2e471e
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2024-11-25 12:29:49 +0530

    Update Project

--- frontend/src/components/YTclone/contstant/YouTube.js
+++ frontend/src/components/YTclone/contstant/YouTube.js
@@ -1,8 +1,7 @@
-// export const API_KEY = "AIzaSyAq7owgLQA25uGWQMoiPgtYxC0vEnrCz74demo"
-// export const API_KEY = "AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8demo"
-// export const API_KEY = "AIzaSyDc453ALa15opv2sr36yyKWJB7iptg19QAdemo"
+// export const API_KEY = "AIzaSyAq7owgLQA25uGWQMoiPgtYxC0vEnrCz74"
+// export const API_KEY = "AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8"
+export const API_KEY = "AIzaSyDc453ALa15opv2sr36yyKWJB7iptg19QA"
 
-// AIzaSyDc453ALa15opv2sr36yyKWJB7iptg19QA
 export  const YouTube_Videos = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=IN&key=${API_KEY}`;
 
 export const channel = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id="UC5c9VlYTSvBSCaoMu_GI6gQ"&key=${API_KEY}`
